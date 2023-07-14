3ea18ccb27b3f6f54c6702561236fbe40ae63ef8
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-07-14 21:18:14 +0300

    Comment css appbar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,7 +1,7 @@
 import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import { AuthNav } from 'components/AuthNav/AuthNav';
-import css from './AppBar.module.css';
+// import css from './AppBar.module.css';
 
 export function AppBar() {
     return (
