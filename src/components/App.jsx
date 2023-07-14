c1a9abdbf62fe9a749ec70846444db999141f202
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-07-14 21:13:44 +0300

    Del comment code from App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,6 @@ import { Routes, Route } from 'react-router-dom';
 import css from './App.module.css';
 import { Layout } from './Layout';
 import { lazy } from 'react';
-// import { Home } from 'pages/Home';
-// import { Login } from 'pages/Login';
-// import { Register } from 'pages/Register';
-// import { Contacts } from 'pages/Contacts';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
@@ -14,6 +10,7 @@ const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export function App() {
   return (
+
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
@@ -25,45 +22,3 @@ export function App() {
   );
 };
 
-// import { ContactForm } from './ContactForm/ContactForm';
-// import { Filter } from './Filter/Filter';
-// import { ContactList } from './ContactList/ContactList';
-// import css from './App.module.css';
-// import { useSelector } from 'react-redux/es/hooks/useSelector';
-// import { getContacts, getError, getFilter, getIsLoading } from 'redux/selectors';
-// import { useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import { fetchContacts } from 'redux/operations';
-
-// import { Spinner } from './Spinner/Spinner';
-
-// export function App() {
-//   const dispatch = useDispatch();
-//   const isLoading = useSelector(getIsLoading);
-//   const error = useSelector(getError);
-//     const filter = useSelector(getFilter);
-//     const contacts = useSelector(getContacts);
-
-//   useEffect(() => { dispatch(fetchContacts()); }, [dispatch]);
-
-//   const normFilter = filter.toLocaleLowerCase();
-//   const findContacts = contacts.filter(contact =>
-//       contact.name.toLowerCase().includes(normFilter)
-//     );
-
-//   return (
-//     <div className={css.container}>
-//       <h1 className={css.title}>
-//         Phone<span className={css.accent}>book</span>
-//       </h1>
-//       <ContactForm contacts={findContacts} />
-
-//       <h2 className={css.subtitle}>Contacts</h2>
-//       <Filter />
-//       {isLoading && <Spinner/>}
-//       {contacts && !error && (
-//         <ContactList findContacts={findContacts} />
-//       )}
-//     </div>
-//   );
-// }
